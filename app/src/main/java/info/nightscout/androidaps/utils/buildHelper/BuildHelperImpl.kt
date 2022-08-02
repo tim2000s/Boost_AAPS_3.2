0fac1f5adf97edea882028d473c9b48a981ec3e5
Sergey Zorchenko <sezorchenko@ozon.ru>
2022-08-02 17:00:57 +0300

    revert IsDev to original

--- app/src/main/java/info/nightscout/androidaps/utils/buildHelper/BuildHelperImpl.kt
+++ app/src/main/java/info/nightscout/androidaps/utils/buildHelper/BuildHelperImpl.kt
@@ -21,7 +21,7 @@ class BuildHelperImpl constructor(
         val engineeringModeSemaphore = File(fileListProvider.ensureExtraDirExists(), "engineering_mode")
 
         engineeringMode = engineeringModeSemaphore.exists() && engineeringModeSemaphore.isFile || sp.getBoolean(R.string.key_engineering_mode, false)
-        devBranch = true
+        devBranch = BuildConfig.VERSION.contains("-") || BuildConfig.VERSION.matches(Regex(".*[a-zA-Z]+.*"))
     }
 
     override fun isEngineeringModeOrRelease(): Boolean =
