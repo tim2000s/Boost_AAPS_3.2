9fefd5dcca35eca8b05f54d9c740f7eb925158c6
DICKO <mark@servecold.net>
2022-09-30 07:09:49 +1000

    eBGw for COB UAM bug

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1188,8 +1188,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             // positive or negative delta with acceleration and lower eBG uses TBR - generally for stubborn high bg
             sens_predType = (DeltaPct > 1.0 && eventualBG < bg ? "TBR" : sens_predType);
 
-            // SAFETY: when not accelerating use BG
-            sens_predType = (DeltaPct <= 1.0 ? "BG" : sens_predType);
+            // SAFETY: when not accelerating use TBR
+            // sens_predType = (DeltaPct <= 1.0 ? "BG" : sens_predType);
             //sens_predType = (DeltaPct <= 1.0 && eventualBG > bg ? "TBR" : sens_predType);
             // SAFETY: high bg with high delta uses current bg, attempts to reduce overcorrection with fast acting carbs
             sens_predType = (bg > ISFbgMax && delta >= 9 && eventualBG > bg? "BG" : sens_predType);
@@ -1199,7 +1199,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         if (sens_predType == "COB" || (sens_predType == "UAM" && COB)) {
             // positive or negative delta with acceleration and UAM default
             eBGweight = (DeltaPct > 1.0 && sens_predType == "COB" ? 0.50 : 0.25);
-            eBGweight = (DeltaPct > 1.0 && sens_predType == "UAM" ? 0.50 : 0.25);
+            eBGweight = (DeltaPct > 1.0 && sens_predType == "UAM" ? 0.50 : eBGweight);
             // positive or negative delta with acceleration and lower eBG uses current BG - generally for stubborn high bg
             sens_predType = (DeltaPct > 1.0 && eventualBG < bg ? "TBR" : sens_predType);
 
