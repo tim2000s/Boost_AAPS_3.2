a5fe2dac55c3ed688bfcbd6ea8879fda1874753e
Sergey Zorchenko <sezorchenko@ozon.ru>
2023-09-14 16:16:01 +0300

    wear version fix remove tdd dependency for DynISMSMB

--- plugins/aps/src/main/java/info/nightscout/plugins/aps/openAPSSMBDynamicISF/OpenAPSSMBDynamicISFPlugin.kt
+++ plugins/aps/src/main/java/info/nightscout/plugins/aps/openAPSSMBDynamicISF/OpenAPSSMBDynamicISFPlugin.kt
@@ -76,8 +76,5 @@ class OpenAPSSMBDynamicISFPlugin @Inject constructor(
     }
 
     // If there is no TDD data fallback to SMB as ISF calculation may be really off
-    override fun provideDetermineBasalAdapter(): DetermineBasalAdapter =
-        if (tdd1D == null || tdd7D == null || tddLast4H == null || tddLast8to4H == null || tddLast24H == null || !dynIsfEnabled.value())
-            DetermineBasalAdapterSMBJS(ScriptReader(context), injector)
-        else DetermineBasalAdapterSMBDynamicISFJS(ScriptReader(context), injector)
+    override fun provideDetermineBasalAdapter(): DetermineBasalAdapter = DetermineBasalAdapterSMBDynamicISFJS(ScriptReader(context), injector)
 }
