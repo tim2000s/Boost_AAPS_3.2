b644faeea4960f86bb0095041892a61b9a2212aa
Milos Kozak <m.kozak@sysop.cz>
2023-10-17 01:08:34 +0300

    optimize db queries

--- database/impl/src/main/java/app/aaps/database/impl/AppRepository.kt
+++ database/impl/src/main/java/app/aaps/database/impl/AppRepository.kt
@@ -225,11 +225,6 @@ import kotlin.math.roundToInt
             .map { if (!ascending) it.reversed() else it }
             .subscribeOn(Schedulers.io())
 
-    @Suppress("unused")
-    fun getModifiedTemporaryTargetsDataFromId(lastId: Long): Single<List<TemporaryTarget>> =
-        database.temporaryTargetDao.getModifiedFrom(lastId)
-            .subscribeOn(Schedulers.io())
-
     fun getTemporaryTargetActiveAt(timestamp: Long): Single<ValueWrapper<TemporaryTarget>> =
         database.temporaryTargetDao.getTemporaryTargetActiveAt(timestamp)
             .subscribeOn(Schedulers.io())
@@ -496,11 +491,6 @@ import kotlin.math.roundToInt
                 }
             }
 
-    @Suppress("unused")
-    fun getModifiedBolusesDataFromId(lastId: Long): Single<List<Bolus>> =
-        database.bolusDao.getModifiedFrom(lastId)
-            .subscribeOn(Schedulers.io())
-
     fun getLastBolusRecord(): Bolus? =
         database.bolusDao.getLastBolusRecord()
 
@@ -731,11 +721,6 @@ import kotlin.math.roundToInt
                         .map { it to nextIdElement }
                 }
             }
-
-    @Suppress("unused")
-    fun getModifiedTemporaryBasalDataFromId(lastId: Long): Single<List<TemporaryBasal>> =
-        database.temporaryBasalDao.getModifiedFrom(lastId)
-            .subscribeOn(Schedulers.io())
 
     fun getTemporaryBasalActiveAt(timestamp: Long): Single<ValueWrapper<TemporaryBasal>> =
         database.temporaryBasalDao.getTemporaryBasalActiveAt(timestamp)
