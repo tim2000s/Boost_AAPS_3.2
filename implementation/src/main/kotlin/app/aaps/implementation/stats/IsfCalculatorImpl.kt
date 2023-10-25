82c22aa17789538e68467a8cb3c8151b0eedade4
t_j_s <street.tj@gmail.com>
2023-10-25 19:34:23 +0100

    If weighted 8h TDD < 0.75 TDD7D, use 75% of TDD 7 day

--- implementation/src/main/kotlin/app/aaps/implementation/stats/IsfCalculatorImpl.kt
+++ implementation/src/main/kotlin/app/aaps/implementation/stats/IsfCalculatorImpl.kt
@@ -86,11 +86,13 @@ class IsfCalculatorImpl @Inject constructor(
                         val tddLast8to4H = tddCalculator.calculateDaily(-8, -4)?.totalAmount ?: 0.0
                         val tddWeightedFromLast8H = ((1.4 * tddLast4H) + (0.6 * tddLast8to4H)) * 3
                         var tdd =
-                            if (tdd1D != null) (tddWeightedFromLast8H * 0.33) + (tdd7D * 0.34) + (tdd1D * 0.33)
+                            if (tddWeightedFromLast8H < 0.75 * tdd7D) (tdd7D * 0.75)
+                            else if (tdd1D != null) (tddWeightedFromLast8H * 0.33) + (tdd7D * 0.34) + (tdd1D * 0.33)
                             else tddWeightedFromLast8H
 
                         jsLogger.debug("TDD: ${Round.roundTo(tdd, 0.01)}")
                         jsLogger.debug("tdd1D: ${ if (tdd1D == null) null else Round.roundTo(tdd1D, 0.01)}")
+                        jsLogger.debug("tdd7D: ${ if (tdd7D == null) null else Round.roundTo(tdd7D, 0.01)}")
                         jsLogger.debug("tddLast4H: ${Round.roundTo(tddLast4H, 0.01)}")
                         jsLogger.debug("tddLast8to4H: ${Round.roundTo(tddLast8to4H, 0.01)}")
                         jsLogger.debug("tddWeightedFromLast8H: ${Round.roundTo(tddWeightedFromLast8H, 0.01)}")
@@ -113,7 +115,7 @@ class IsfCalculatorImpl @Inject constructor(
                     else jsLogger.debug("tdd7D not found, falling back to profile sensNormalTarget of $sensNormalTarget")
                 }
 
-                val bgTarget = profile.getTargetMgdl();
+                val bgTarget = profile.getTargetMgdl()
                 if (isTempTarget && ((highTemptargetRaisesSensitivity && bgTarget > bgNormalTarget) || (lowTemptargetLowersSensitivity && bgTarget < bgNormalTarget))) {
                     val c = halfBasalTarget - bgNormalTarget
                     ratio = c / (c + bgTarget - bgNormalTarget)
